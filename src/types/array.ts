fcb6e6babe93d01d501f1cc47dfe471a83a95062
marinavasqr23 <marinavasqufc@gmail.com>
2024-05-06 12:58:28 -0300

    Adicionando Array

---
+++ src/types/array.ts
@@ -0,0 +1,18 @@
+//ARRAY: Conjunto de caracteres
+
+let nomes:string[]=['Maria','João','Paulo'];
+let idades:number[]=[19,20,22];
+
+//let nomes:Array<string>=['Maria','João','Paulo'];
+
+//Adicionando novos caracteres
+
+nomes.push('Maria');
+idades.push(23);
+
+console.log(nomes);
+console.log(idades);
+
+//[ 'Maria', 'João', 'Paulo', 'Maria' ]
+//[ 19, 20, 22, 23 ]
+
