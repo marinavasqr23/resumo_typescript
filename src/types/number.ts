bb19fd2f7c3b1a68226dd585e8022de8ea3b3a8b
marinavasqr23 <marinavasqufc@gmail.com>
2024-05-06 12:36:29 -0300

    Adicionando Number

---
+++ src/types/number.ts
@@ -0,0 +1,11 @@
+//NUMBER: tipagem para todos tipos de valores
+
+let octal: number = 0o745;
+let binary: number = 0b1111;
+let decimal: number = 34;
+let hex: number = 0xf34d;
+
+let octal1 = 0o745;
+let binary1 = 0b1111;
+let decimal1 = 34;
+let hex1 = 0xf34d;
